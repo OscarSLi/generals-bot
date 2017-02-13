47353b3de5f7611da40479261f9d11c2a4d23fd8
Oscar Li <oscar.siyuan.li@gmail.com>
2017-02-12 22:32:00 -0500

    Refactoring + put user_id in process.env

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 var io = require('socket.io-client');
 var socket = io('http://botws.generals.io');
 
+// Terrain Constants.
+// Any tile with a nonnegative value is owned by the player corresponding to its value.
+// For example, a tile with value 1 is owned by the player with playerIndex = 1.
+var TILE_EMPTY = -1;
+var TILE_MOUNTAIN = -2;
+var TILE_FOG = -3;
+var TILE_FOG_OBSTACLE = -4; // Cities and Mountains show up as Obstacles in the fog of war.
+
+// Game data.
+var playerIndex;
+var generals; // The indicies of generals we have vision of.
+var cities = []; // The indicies of cities we have vision of.
+var map = [];
+
 socket.on('disconnect', function() {
     console.error('Disconnected from server.');
     process.exit(1);
@@ -9,21 +23,15 @@ socket.on('disconnect', function() {
 socket.on('connect', function() {
     console.log('Connected to server.');
 
-    /* Don't lose this user_id or let other people see it!
-     * Anyone with your user_id can play on your bot's account and pretend to be your bot.
-     * If you plan on open sourcing your bot's code (which we strongly support), we recommend
-     * replacing this line with something that instead supplies the user_id via an environment variable, e.g.
-     * var user_id = process.env.BOT_USER_ID;
-     */
-    var user_id = 'my_example_bot_id';
-    var username = 'Example Bot';
+    var user_id = process.env.BOT_USER_ID;
+    var username = 'Oscar_Bot';
 
     // Set the username for the bot.
     socket.emit('set_username', user_id, username);
 
     // Join a custom game and force start immediately.
     // Custom games are a great way to test your bot while you develop it because you can play against your bot!
-    var custom_game_id = 'my_private_game';
+    var custom_game_id = 'OscarGame';
     socket.emit('join_private', custom_game_id, user_id);
     socket.emit('set_force_start', custom_game_id, true);
     console.log('Joined custom game at http://bot.generals.io/games/' + encodeURIComponent(custom_game_id));
@@ -40,20 +48,6 @@ socket.on('connect', function() {
     // Join a 2v2 team.
     // socket.emit('join_team', 'team_name', user_id);
 });
-
-// Terrain Constants.
-// Any tile with a nonnegative value is owned by the player corresponding to its value.
-// For example, a tile with value 1 is owned by the player with playerIndex = 1.
-var TILE_EMPTY = -1;
-var TILE_MOUNTAIN = -2;
-var TILE_FOG = -3;
-var TILE_FOG_OBSTACLE = -4; // Cities and Mountains show up as Obstacles in the fog of war.
-
-// Game data.
-var playerIndex;
-var generals; // The indicies of generals we have vision of.
-var cities = []; // The indicies of cities we have vision of.
-var map = [];
 
 /* Returns a new array created by patching the diff into the old array.
  * The diff formatted with alternating matching and mismatching segments:
